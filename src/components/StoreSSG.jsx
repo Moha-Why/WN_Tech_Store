8fcbd0b1c8c71dd77965dcbc4c5a19bda0b0bf2f
Mohamed <me878783@gmail.com>
2025-12-30 03:32:37 +0200

    final test

--- src/components/StoreSSG.jsx
+++ src/components/StoreSSG.jsx
@@ -225,6 +225,8 @@ export default function TechStore() {
                       fill
                       className="
                         object-contain p-4
+                        transition-transform duration-500
+                        group-hover:scale-110
                       "
                       sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
                       priority={index < 2}
